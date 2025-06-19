f653bdaef64d8a576a422948b130ee09a58dc4c0
sean <tmdgus717@naver.com>
2025-06-19 19:42:24 +0900

    문자열을 활용한 HTML 다루기

--- essential-javascript/hacker-new-client/app.js
+++ essential-javascript/hacker-new-client/app.js
@@ -1,38 +1,48 @@
 const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; //마킹
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement('ul');
-
-window.addEventListener('hashchange', function() {
-  const id = location.hash.substr(1);
-
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+function getData(url){
+  ajax.open('GET', url, false);
   ajax.send();
 
-  const newsContent = JSON.parse(ajax.response);
-  const title = document.createElement('h1');
+  return JSON.parse(ajax.response);
+}
 
+const newsFeed = getData(NEWS_URL);
+const ul = document.createElement('ul');
+
+//브라우저가 제공해주는 event system
+//ui 요소가 / 이벤트가발생했을때 / 함수를 호출해줘..
+// # : 해시 -> 북마크 -> hashchange 이벤트
+window.addEventListener('hashchange', function() {
+  console.log("change hash")
+  //location : 주소와 관련된 정보를 제공하는 객체
+  //#자르고 아이디값만 가져오기
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+  
+  const title = document.createElement('h1');
   title.innerHTML = newsContent.title;
 
   content.appendChild(title);
 });
 
 for(let i = 0; i < 10; i++) {
+  const div = document.createElement('div');//temp tag
   const li = document.createElement('li');
   const a = document.createElement('a');
 
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})  
+      </a>
+    </li>
+  `
 
-  li.appendChild(a);
-  ul.appendChild(li);
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
